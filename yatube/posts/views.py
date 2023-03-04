8585f2afe4be6fa8a807dca57e87ae3d1de955df
Максим Соловьев <maximnoch@gmail.com>
2023-03-04 22:45:38 +0300

    Вроде работает

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import  HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Если честно хз что сюда писать')
+
+
+def group_posts(request, pk):
+    return HttpResponse(f'Оригинальный пост №{pk}')
